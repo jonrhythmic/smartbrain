130f92f1e37941274f8f91156d8ac1fdde3b0af1
--global <jon.a.grenness@gmail.com>
2021-02-02 23:00:37 +0100

    connected frontend to backend

--- src/containers/App.js
+++ src/containers/App.js
@@ -131,8 +131,8 @@ class App extends Component {
                     </>
                     : (
                         route === 'signin' 
-                        ? <Signin onRouteChange={this.onRouteChange}/>
-                        : <Register onRouteChange={this.onRouteChange}/>
+                        ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+                        : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
                     )
                 }
             </div>
